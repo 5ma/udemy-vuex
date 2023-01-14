538d6c36bb2f70536d792ad12b8fd4f10a02137d
Komakine Anna <bbxxuw@gmail.com>
2023-01-14 22:47:12 +0900

    ✨ コンポーネントのグローバル登録とローカル登録を試してみる

---
+++ src/LikeNumber.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button @click="increment">いいね！({{ number }})</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.number++;
+    }
+  }
+}
+</script>
