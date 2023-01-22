350c939e0a896485580d11f4840bedfed5451ab6
Komakine Anna <bbxxuw@gmail.com>
2023-01-22 23:25:42 +0900

    ✨ 名前付きビューを使って、複数のrouter-viewを置いてみる

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,22 @@
 <template>
   <div>
-    <nav>
-      <router-link
-       to="/"
-       active-class="link-active"
-       exact
-       class="link"
-       >Home</router-link>
-      <!-- tagで好きなタグに置き換えることが出来る -->
-      <router-link
-       to="/users" 
-       tag="div"
-       active-class="link-active"
-       exact
-       class="link"
-       >Users</router-link>
-    </nav>
+    <!-- 名前付きビューを使うことで、複数のrouter-viewを1ページ内で置くことが出来る -->
+    <router-view name="header"></router-view>
     <router-view></router-view>
   </div>
 </template>
 
 <style scoped>
-  .link {
-    width: fit-content;
-    min-width: 150px;
-    background-color: #000;
-    color: #fff;
-    cursor: pointer;
-    text-align: center;
-    text-decoration: underline;
-  }
-  .link-active {
-    font-size: 20px;
-  }
+.link {
+  width: fit-content;
+  min-width: 150px;
+  background-color: #000;
+  color: #fff;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: underline;
+}
+.link-active {
+  font-size: 20px;
+}
 </style>
---
+++ src/views/HeaderUsers.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav>
+    <h2>Users navigation</h2>
+    <router-link to="/" active-class="link-active" exact class="link">Home</router-link>
+    <!-- tagで好きなタグに置き換えることが出来る -->
+    <router-link to="/users" tag="div" active-class="link-active" exact class="link">Users</router-link>
+  </nav>
+</template>
