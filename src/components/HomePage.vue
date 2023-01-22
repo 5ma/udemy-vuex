29c0b272ec565fde58ae725478fc03b22bcebada
Komakine Anna <bbxxuw@gmail.com>
2023-01-22 11:29:59 +0900

    ✨ カスタムディレクティブをローカルに登録してみる

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,3 +1,19 @@
 <template>
-  <p v-border:dotted="{ width: '12px', color: 'lightBlue' }">Home</p>
+  <p v-border:solid.round.shadow="{ width: '12px', color: 'lightBlue' }">Home</p>
 </template>
+
+<script>
+export default {
+  directives: {
+    border(el, binding) {
+      el.style.border = `${binding.arg} ${binding.value.color} ${binding.value.width}`;
+      if (binding.modifiers.round) {
+        el.style.borderRadius = "0.5rem";
+      }
+      if (binding.modifiers.shadow) {
+        el.style.boxShadow = "0 2px 5px rgba(0,0,0,0.3)";
+      }
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import LikeNumber from "./components/LikeNumber.vue";
 
 Vue.config.productionTip = false;
 Vue.component("LikeNumber", LikeNumber);
-Vue.directive("border", (el, binding) => {
-  el.style.border = `${binding.arg} ${binding.value.color} ${binding.value.width}`;
-});
 
 new Vue({
   render: (h) => h(App),
